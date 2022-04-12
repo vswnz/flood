aa6b654dee8828ca70223d16f2536cf13ed9c669
vswnz <vsw@whs.school.nz>
2022-04-12 12:18:44 +1200

    added stub level2

---
+++ level2.java
@@ -0,0 +1,32 @@
+/**
+ * Level 2 *
+ * 
+ * There are several tasks to accomplish.  Try them one at a time.
+ * They are named task1, task2, etc.
+ * 
+ * 
+ * Don't forget to fill in your name and details.
+ * 
+ * @author:   Put your name in here
+ * @version:  Put todays date in here.
+ */
+
+class level2{
+    int task=0; // which task are we working on at the moment?
+
+    public int[][] board1 = new int[2][2];
+    public char[][] board2 ;
+    public float[][] board3 ;
+    public boolean[][]  board4 ;
+    public long[][]  board5 ;
+
+    
+    
+    
+    
+    
+    
+    
+    /* You don't need to edit anything below here */
+    level2(){}
+}
